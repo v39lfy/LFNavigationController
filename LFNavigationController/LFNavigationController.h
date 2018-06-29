4da0d9ec0b7552261ba3983ba82853dc50dca0b9
v39lfy <admin@onlywish.me>
2018-06-29 11:27:18 +0800

    init

---
+++ LFNavigationController/LFNavigationController.h
@@ -0,0 +1,19 @@
+//
+//  LFNavigationController.h
+//  LFNavigationController
+//
+//  Created by 福有李 on 2018/6/29.
+//  Copyright © 2018年 福有李. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LFNavigationController.
+FOUNDATION_EXPORT double LFNavigationControllerVersionNumber;
+
+//! Project version string for LFNavigationController.
+FOUNDATION_EXPORT const unsigned char LFNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LFNavigationController/PublicHeader.h>
+
+
